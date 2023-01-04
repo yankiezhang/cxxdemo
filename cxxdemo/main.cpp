706c744994e6e1f213826a5e4d69ac9e8b25caf5
Yang <yankiezhang@gmail.com>
2023-01-04 22:11:48 +0800

    Initial Commit

---
+++ cxxdemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cxxdemo
+//
+//  Created by Yang Zhang on 2023/1/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
